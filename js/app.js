98f5649665aabfec988dc1a1190d3b4afdfd5935
Ismail <ismailsardar540@gmail.com>
2022-02-17 00:32:43 +0600

    javascript for slide card use slick-website

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.container-slaider_food').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidersToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+  });
+
