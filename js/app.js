c898c20ea0f8206fc4c020923b8529cfca4e7186
Ismail <ismailsardar540@gmail.com>
2022-02-19 01:40:59 +0600

    card slider for js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
         slidersToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
     });
 
     $('.nav-tegair').click(function(){
